dd7a99ac77c6c97a9cec187d745c1dc0d5e05571
jackhoang <chun.hoang@accenture.com>
2016-03-24 09:14:16 -0400

    Add 'Register' button

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -6,23 +6,24 @@ import TextInput from './TextInput'
 
 class RegistrationForm extends Component {
   render() {
-    const {fields: {userName, email, phone}, handleSubmit} = this.props;
+    const {fields: {userName, email}, handleSubmit} = this.props;
     return (
       <div>
         <h1>User Registration Form</h1>
         <form onSubmit={handleSubmit}>
           <div><TextInput label='Name' id='userName' /></div>
           <div><TextInput label='Email' id='email' /></div>
-          <div><TextInput label='Phone Number' id='phone' /></div>
+          <div><button>Register</button></div>
         </form>
       </div>
     );
   }
+
 }
 
 RegistrationForm = reduxForm({
   form: 'registration',
-  fields: ['userName', 'email', 'phone']
+  fields: ['userName', 'email']
 })(RegistrationForm);
 
 export default RegistrationForm;
