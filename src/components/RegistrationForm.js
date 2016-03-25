4905233f64ca33c228d7691ee98165c37cb863cb
jackhoang <chun.hoang@accenture.com>
2016-03-25 17:39:17 -0400

    Changes Register button to a submit button

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,19 +1,29 @@
-import React, {Component} from 'react';
-import {reduxForm} from 'redux-form';
+import React, {Component, PropTypes} from 'react'
+import {reduxForm} from 'redux-form'
 
 import FormField from './FormField'
 import TextInput from './TextInput'
 
 class RegistrationForm extends Component {
+  static propTypes = {
+    fields: PropTypes.object.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    error: PropTypes.string,
+    submitting: PropTypes.bool.isRequired
+  }
+
   render() {
-    const {fields: {userName, email}, handleSubmit} = this.props;
+    const {fields: {userName, email}, handleSubmit, error, submitting} = this.props;
     return (
       <div>
         <h1>User Registration Form</h1>
         <form onSubmit={handleSubmit}>
           <div><TextInput label='Name' id='userName' /></div>
           <div><TextInput label='Email' id='email' /></div>
-          <div><button>Register</button></div>
+          <div><button type="submit" disabled={submitting}>
+                  Register
+              </button>
+          </div>
         </form>
       </div>
     );
@@ -26,4 +36,4 @@ RegistrationForm = reduxForm({
   fields: ['userName', 'email']
 })(RegistrationForm);
 
-export default RegistrationForm;
+export default RegistrationForm
