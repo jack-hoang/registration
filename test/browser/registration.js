970bc52f9839fab3a554c6e916adfc2e5542f66b
jackhoang <chun.hoang@accenture.com>
2016-03-26 15:22:12 -0400

    Adds test for showing verification page

--- test/browser/registration.js
+++ test/browser/registration.js
@@ -19,5 +19,30 @@ module.exports = {
     client.expect.element('button[type=submit]').text.to.equal('Register');
 
     client.end();
+  },
+
+  'Submit User Registration Form' : function(client) {
+    client
+      .url('http://localhost:3000/')
+      .pause(1000);
+
+    client.setValue('#userName', 'John Doe');
+    client.setValue('#email', 'john.doe@example.com');
+    client.click('button[type=submit]');
+    client.pause(1000);
+
+    client.assert.containsText('#app', 'Verify User information');
+
+    client.expect.element('#userName').to.be.present;
+    client.expect.element('#userName').to.be.an('div');
+
+    client.expect.element('#email').to.be.present;
+    client.expect.element('#email').to.be.an('div');
+
+    client.expect.element('button[name=edit]').to.be.present;
+    client.expect.element('button[name=verify]').to.be.present;
+
+    client.end();
   }
+
 }
