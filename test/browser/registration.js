4998736309ad84d39ce0a7df7e7ba7a7b9b8d0eb
jackhoang <chun.hoang@accenture.com>
2016-03-30 01:12:02 -0400

    Updated user stories and registration test

--- test/browser/registration.js
+++ test/browser/registration.js
@@ -1,4 +1,5 @@
 module.exports = {
+
   'User Registration Form' : function (client) {
     client
       .url('http://localhost:3000/')
@@ -7,7 +8,7 @@ module.exports = {
     // expect element to be present in 1000ms
     client.expect.element('body').to.be.present.before(1000);
 
-    client.assert.containsText('#app', 'User Registration Form');
+    client.expect.element('#title').text.to.contain('User Registration Form');
 
     client.expect.element('#userName').to.be.present;
     client.expect.element('#userName').to.be.an('input');
@@ -15,23 +16,23 @@ module.exports = {
     client.expect.element('#email').to.be.present;
     client.expect.element('#email').to.be.an('input');
 
-    client.expect.element('button[type=submit]').to.be.present;
-    client.expect.element('button[type=submit]').text.to.equal('Register');
+    client.expect.element('button').to.be.present;
+    client.expect.element('button').text.to.equal('Next');
 
     client.end();
   },
 
-  'Submit User Registration Form' : function(client) {
+  'Clicking Next button' : function(client) {
     client
       .url('http://localhost:3000/')
       .pause(1000);
 
     client.setValue('#userName', 'John Doe');
     client.setValue('#email', 'john.doe@example.com');
-    client.click('button[type=submit]');
+    client.click('button');
     client.pause(1000);
 
-    client.assert.containsText('#app', 'Verify User information');
+    client.expect.element('#title').text.to.contain('Verify Registration Information');
 
     client.expect.element('#userName').to.be.present;
     client.expect.element('#userName').to.be.a('div');
@@ -40,7 +41,28 @@ module.exports = {
     client.expect.element('#email').to.be.a('div');
 
     client.expect.element('button[id=edit]').to.be.present;
-    client.expect.element('button[id=confirm]').to.be.present;
+    client.expect.element('button[id=register]').to.be.present;
+
+    client.end();
+  },
+
+  'Editing registration info' : function(client) {
+    client
+      .url('http://localhost:3000/')
+      .pause(1000);
+
+    client.setValue('#userName', 'John Doe');
+    client.setValue('#email', 'john.doe@example.com');
+    client.click('button');
+    client.pause(1000);
+
+    client.expect.element('#title').text.to.contain('Verify Registration Information');
+
+    client.expect.element('button[id=edit]').to.be.present;
+    client.click('button[id=edit]');
+    client.pause(1000);
+
+    client.expect.element('#title').text.to.contain('User Registration Form');
 
     client.end();
   }
