d1692eb5fa91932ff2ac0777aa28ba139fed2b8f
jackhoang <chun.hoang@accenture.com>
2016-03-30 01:47:25 -0400

    Adds test for new REGISTER action

--- src/reducers/registrationReducer-spec.js
+++ src/reducers/registrationReducer-spec.js
@@ -2,13 +2,13 @@ import registrationReducer from './registrationReducer'
 import * as actions from '../actions/actions'
 import * as types from '../actions/ActionTypes'
 
-describe('registration reducer', () => {
+describe('When registration reducer receives actions', () => {
 
-  it('should return the initial state', () => {
+  it('should return the initial state if no action is received', () => {
     expect(registrationReducer(undefined, {})).toEqual({})
   })
 
-  it('should handle '+types.FORM_UPDATE, () => {
+  it('should return registration info when action type is '+types.FORM_UPDATE, () => {
     const formData = {
       userName: 'John Doe',
       email: 'john.doe@example.com'
@@ -21,6 +21,23 @@ describe('registration reducer', () => {
       ).toEqual(
         formData
       )
+  })
+
+  it('should return registration status when action type is '+types.REGISTER, () => {
+    const formData = {
+      userName: 'John Doe',
+      email: 'john.doe@example.com'
+    }
+
+    expect(
+      registrationReducer(
+        {},
+        actions.register(formData))
+      ).registrationStatus.toEqual(
+        "Thank you " + formData.userName
+            + "! You have successfully registered."
+      )
+
   })
 
 })
