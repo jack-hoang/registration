2c2fab69de2778a4e6e44e4f58e81c4755842d2a
jackhoang <chun.hoang@accenture.com>
2016-03-30 02:31:17 -0400

    Adds logic to reduce REGISTER actions

--- src/reducers/registrationReducer.js
+++ src/reducers/registrationReducer.js
@@ -3,14 +3,17 @@ import * as types from '../actions/ActionTypes'
 const initialState = {}
 
 export default function registrationReducer(state = {}, action) {
+  const data = action.payload
   switch (action.type) {
     case types.FORM_UPDATE:
-      const data = action.payload
       return Object.assign({}, state, {
         userName: data.userName,
         email: data.email
       })
-      break;
+    case types.REGISTER:
+      return Object.assign({}, state,
+        data, {registrationStatus: "Thank you " + data.userName
+            + "! You have successfully registered."})
     default:
       return state;
   }
