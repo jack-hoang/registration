cc21073bb678fd9d36250d4efd1f3507f3ff6616
jackhoang <chun.hoang@accenture.com>
2016-03-30 23:01:37 -0400

    Support passing message in as a prop

--- src/components/RegistrationStatusInfo.js
+++ src/components/RegistrationStatusInfo.js
@@ -4,8 +4,6 @@ import Row from 'react-bootstrap/lib/Row'
 import Col from 'react-bootstrap/lib/Col'
 
 export default class RegistrationStatusInfo extends Component {
-  static propTypes = {
-  }
 
   constructor(props) {
     super(props);
@@ -13,14 +11,14 @@ export default class RegistrationStatusInfo extends Component {
   }
 
   render() {
-    const { message } = this.props
+    const { statusMessage } = this.props.state
     return (
       <Grid>
       <Row>
         <Col md={12} id='title'>Registration Status</Col>
       </Row>
       <Row>
-        <Col md={12} id='message'>{message}</Col>
+        <Col md={12} id='message'>{statusMessage}</Col>
       </Row>
       </Grid>
     )
